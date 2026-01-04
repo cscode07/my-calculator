f3d58da4f9fc1b09e96a7f89cba15d90ff77893a
cscode07 <claudiocode2025@gmail.com>
2026-01-04 01:58:59 +0000

    add def main

--- run.py
+++ run.py
@@ -25,3 +25,27 @@ def calculate(option, num1, num2):
         if num2 == 0:
             return "❌ Error: division by zero."
         return num1 / num2
+    
+def main():
+    print("Welcome to the Python Calculator!")
+
+    while True:
+        show_menu()
+        option = input("Choose an option (1-5): ")
+
+        if option == "5":
+            print("Thank you for using the calculator 👋")
+            break
+
+        if option not in ["1", "2", "3", "4"]:
+            print("❌ Invalid option.")
+            continue
+
+        num1 = get_number("Enter the first number: ")
+        num2 = get_number("Enter the second number: ")
+
+        result = calculate(option, num1, num2)
+        print(f"Result: {result}")
+
+
+main()
