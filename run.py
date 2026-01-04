46a5a90ac16f4c7a246ef68c968fe4e01a964192
cscode07 <claudiocode2025@gmail.com>
2026-01-04 01:57:01 +0000

    add def calculate option num1 num2

--- run.py
+++ run.py
@@ -6,9 +6,22 @@ def show_menu():
     print("4. Division (/)")
     print("5. Exit")
 
-    def get_number(message):
+def get_number(message):
     while True:
         try:
             return float(input(message))
         except ValueError:
             print("❌ Please enter a valid number.")
+
+
+def calculate(option, num1, num2):
+    if option == "1":
+        return num1 + num2
+    elif option == "2":
+        return num1 - num2
+    elif option == "3":
+        return num1 * num2
+    elif option == "4":
+        if num2 == 0:
+            return "❌ Error: division by zero."
+        return num1 / num2
