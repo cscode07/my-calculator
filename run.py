35dd9b629f08b73ee3058b78689f62b1dc2a0a75
cscode07 <claudiocode2025@gmail.com>
2026-01-06 01:23:48 +0000

    final code aproved and pep8 errors fixed

--- run.py
+++ run.py
@@ -8,12 +8,14 @@ def show_menu():
     print("4. Division (/)")
     print("5. Exit")
 
+
 def get_number(message):
     while True:
         try:
             return float(input(message))
         except ValueError:
             print("❌ Please enter a valid number.")
+
 
 def calculate(option, num1, num2):
     if option == "1":
@@ -26,6 +28,7 @@ def calculate(option, num1, num2):
         if num2 == 0:
             return "❌ Error: division by zero."
         return num1 / num2
+
 
 def main():
     print("Welcome to the Python Calculator!")
@@ -47,6 +50,7 @@ def main():
 
         result = calculate(option, num1, num2)
         print(f"Result: {result}")
+
 
 if __name__ == "__main__":
     main()
